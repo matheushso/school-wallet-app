f58f63308ab5825c5cd3ca4b2bbeafacb7a2683a
Matheus Oliveira <matheushso99@hotmail.com>
2023-09-07 11:19:05 -0300

    first commit

---
+++ src/main/java/br/com/app/wallet/school/SchoolWalletAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.app.wallet.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolWalletAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolWalletAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/app/wallet/school/SchoolWalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.wallet.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolWalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
